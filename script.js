290e7bfcb533c8d4c16789e2858bd18f83dd6b03
victor-blanchard <159810800+victor-blanchard@users.noreply.github.com>
2024-11-01 15:29:20 +0100

    e

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://whatyearisit-backend-topaz-six.vercel.app//year")
+fetch("https://whatyearisit-backend-topaz-six.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
